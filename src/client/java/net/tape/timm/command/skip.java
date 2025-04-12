06a5b57aaa7ac5dba6c57b5d1047ce6b454d470f
charlyb01 <charly5bollinger@gmail.com>
2025-04-12 21:21:40 +0200

    v1.0.5 update

--- src/client/java/net/tape/timm/command/skip.java
+++ src/client/java/net/tape/timm/command/skip.java
@@ -1,64 +1,90 @@
 package net.tape.timm.command;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.context.CommandContext;
-import net.minecraft.client.sound.PositionedSoundInstance;
-import net.minecraft.client.sound.Sound;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.util.Identifier;
-import net.tape.timm.songControls;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.tape.timm.timmMain;
+import net.minecraft.util.Identifier;
+import net.minecraft.sound.SoundEvent;
+
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
 
+import net.tape.timm.songControls;
+import net.tape.timm.util.SpotifyLinks;
+
 public class skip {
 
-    // Credit to Earthcomputer for writing most of the command handling code
-    // I couldn't have done command interface shit if it weren't for clientcommands being open source
-    // Earthcomputer a real one for that
-    // https://github.com/Earthcomputer/clientcommands
-
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
-
         dispatcher.register(literal("skip")
-                    // literal skip with no arguments executes skipNoSong
-            .executes(ctx -> {
-                final FabricClientCommandSource src = ctx.getSource();
-                return skipNoSong(src);
-            }) // declare argument with name 'song' and type String
-            .then(argument("song", StringArgumentType.string())
+                .requires(fabricClientCommandSource -> true)
                 .executes(ctx -> {
                     final FabricClientCommandSource src = ctx.getSource();
-                    final String songName =  StringArgumentType.getString(ctx, "song");
-                    return skipWithSong(src, songName);
+                    return skipNoSong(src);
                 })
-            ));
-        
+                .then(argument("song", StringArgumentType.string())
+                        .executes(ctx -> {
+                            final FabricClientCommandSource src = ctx.getSource();
+                            final String songName = StringArgumentType.getString(ctx, "song");
+                            return skipWithSong(src, songName);
+                        })
+                ));
     }
 
     private static int skipNoSong(FabricClientCommandSource source) {
         songControls.skip();
-        source.sendFeedback(Text.translatable("timm.commands.skip.success", songControls.lastSoundInstance.getSound().getIdentifier().toString()));
+
+        String song = songControls.nowPlaying();
+        String songURL = SpotifyLinks.get(song);
+
+        if (song != null && songURL != null) {
+            Text songLink = Text.literal(song)
+                    .setStyle(Style.EMPTY.withColor(0x1ABA53)
+                            .withUnderline(true)
+                            .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, songURL)));
+
+            source.sendFeedback(Text.literal("Now playing: ").append(songLink));
+        } else if (song != null) {
+            source.sendFeedback(Text.literal("Now playing: " + song));
+        } else {
+            source.sendFeedback(Text.literal("Nothing is currently playing."));
+        }
+
         return Command.SINGLE_SUCCESS;
     }
 
-    private static int skipWithSong(FabricClientCommandSource source, String song) {
+    private static int skipWithSong(FabricClientCommandSource source, String songName) {
+        Identifier id = Identifier.tryParse(songName);
 
-        Identifier id = Identifier.tryParse(song);
-        if (id != null && id.getClass().getTypeName().equals("SoundEvent")) {
+        if (id != null) {
             SoundEvent sound = SoundEvent.of(id);
             songControls.skip(sound);
-            source.sendFeedback(Text.translatable("timm.commands.skip.success", song));
+
+            String song = songControls.nowPlaying();
+            String songURL = SpotifyLinks.get(song);
+
+            if (song != null && songURL != null) {
+                Text songLink = Text.literal(song)
+                        .setStyle(Style.EMPTY.withColor(0x1ABA53)
+                                .withUnderline(true)
+                                .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, songURL)));
+
+                source.sendFeedback(Text.literal("Now playing: ").append(songLink));
+            } else if (song != null) {
+                source.sendFeedback(Text.literal("Now playing: " + song));
+            } else {
+                source.sendFeedback(Text.literal("Nothing is currently playing."));
+            }
+
             return Command.SINGLE_SUCCESS;
         } else {
-            source.sendFeedback(Text.translatable("timm.commands.skip.badId", song));
-            return -1; // apparently there isn't a constant defined for command failure???
+            source.sendFeedback(Text.translatable("timm.commands.skip.badId", songName));
+            return -1;
         }
     }
-
 }
+
